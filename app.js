b18b105aefebb3fa574c55a736483de281e9d559
Евгений Остапенко < >
2017-12-21 22:05:20 +0600

    'initial_v.0.3'

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on ip:3000');
 });
